c9c77854375fd01e3349d4a694fa8175eefc439b
Amos Vryhof <amos@vryhofresearch.com>
2021-07-21 08:43:33 -0400

    Get zip code and gazzateer data

--- get-data.sh
+++ get-data.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-locations='US PR VI GU MP AZ'
-LOCATION_BASE=http://download.geonames.org/export/zip/
-#LOCATION_ADDRESS=http://download.geonames.org/export/zip/${location}.zip
-rm *.zip
-rm *.txt
-wget ${LOCATION_BASE}/US.zip
-unzip -o US.zip
-wget ${LOCATION_BASE}/PR.zip
-unzip -o PR.zip
-wget ${LOCATION_BASE}/VI.zip
-unzip -o VI.zip
-wget ${LOCATION_BASE}/GU.zip
-unzip -o GU.zip
-wget ${LOCATION_BASE}/MP.zip
-unzip -o MP.zip
-wget ${LOCATION_BASE}/AZ.zip
-unzip -o AZ.zip
-rm US.zip PR.zip VI.zip GU.zip MP.zip AZ.zip
+python get_data.py
---
+++ get_data.py
@@ -0,0 +1,54 @@
+import os
+import zipfile
+from urllib.request import urlretrieve
+
+countries = ["AZ", "GU", "MP", "PR", "US", "VI"]
+
+
+def get_zip_code_data():
+    file_path = "zip"
+
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
+    for country in countries:
+        print("Get Zip code data for {}".format(country))
+        zip_file = os.path.join(file_path, "{}.zip".format(country))
+        urlretrieve("http://download.geonames.org/export/{}/{}.zip".format(file_path, country), zip_file)
+
+        zip_ref = zipfile.ZipFile(zip_file, "r")
+        zip_ref.extractall(file_path)
+        zip_ref.close()
+
+    for extra_file in ["readme.txt"]:
+        print("Download {}/{}".format(file_path, extra_file))
+        urlretrieve(
+            "http://download.geonames.org/export/{}/{}".format(file_path, extra_file),
+            os.path.join(file_path, extra_file),
+        )
+
+
+def get_gazzetteer_data():
+    file_path = "dump"
+
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
+    for country in countries:
+        print("Get Gazzetteer data for {}".format(country))
+        zip_file = os.path.join(file_path, "{}.zip".format(country))
+        urlretrieve("http://download.geonames.org/export/{}/{}.zip".format(file_path, country), zip_file)
+
+    for extra_file in [
+        "readme.txt",
+        "admin1CodesASCII.txt",
+        "admin2Codes.txt",
+        "alternateNamesV2.zip",
+        "timeZones.txt",
+        "countryInfo.txt",
+    ]:
+        print("Download {}/{}".format(file_path, extra_file))
+        urlretrieve(
+            "http://download.geonames.org/export/{}/{}".format(file_path, extra_file),
+            os.path.join(file_path, extra_file),
+        )
